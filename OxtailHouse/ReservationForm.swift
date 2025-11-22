00199f6be63102fa8ee3401190569884f6ee3ccc
Jay Chestnut <Jaychestnut320@yahoo.com>
2025-11-22 14:31:16 -0500

    assignment 2

---
+++ OxtailHouse/ReservationForm.swift
@@ -0,0 +1,18 @@
+//
+//  ReservationForm.swift
+//  OxtailHouse
+//
+//  Created by Jay Chestnut on 11/19/25.
+//
+
+import SwiftUI
+
+struct ReservationForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReservationForm()
+}
