624ce6d844c69d5099453e7f53f22e4914c62668
Jay Chestnut <Jaychestnut320@yahoo.com>
2025-11-22 14:37:21 -0500

    aaaa

--- OxtailHouse/ReservationForm.swift
+++ OxtailHouse/ReservationForm.swift
@@ -8,8 +8,63 @@
 import SwiftUI
 
 struct ReservationForm: View {
+    let restautantName = "Oxtail House"
+    let maxGuest = 10
+    let maxChildren = 5
+    
+    @State private var userName = ""
+    @State private var guestCount = 1
+    @State private var phoneNumber = ""
+    @State private var previewText = ""
+    @State private var numberChildren = 0
+    @State private var occasion = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section{
+                Text(restautantName)
+                    .font(.title)
+                    .bold()
+                Text("Reservation Form")
+                    .foregroundColor(.secondary)
+            }
+            Section(header: Text("Reservation Details")){
+                TextField("Name", text: $userName)
+                    .textInputAutocapitalization(.words)
+                    .autocorrectionDisabled(true)
+                
+                Stepper("Guest:  \(guestCount)", value: $guestCount, in: 1 ... maxGuest)
+                
+                Stepper("Children: \(numberChildren)", value: $numberChildren, in: 0 ... maxChildren)
+            }
+            
+            Section(header:Text("Contact")){
+                TextField("Phone" , text: $phoneNumber)
+                    .keyboardType(.numberPad)
+            }
+            
+            Section(header:Text("Occasion")){
+                TextField("Occasion", text: $occasion)
+            }
+            
+            Section{
+                Button("Preview reservation"){
+                    previewText = """
+                Name: \(userName)
+                Guest: \(guestCount)
+                Children: \(numberChildren)
+                Phone: \(phoneNumber)
+                Occasion: \(occasion)
+                """
+                }
+            }
+            
+            Section(header:Text("Preview")){
+                Text(previewText)
+                    .font(.footnote)
+                    .foregroundColor(.secondary)
+            }
+        }
     }
 }
 
--- Practices/Variables.playground/Contents.swift
+++ Practices/Variables.playground/Contents.swift
@@ -1,3 +1,37 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//Variables - Values that can change
+//Constants - Do not change
+
+//Constants use "let"
+//variables use "var"
+
+var restaurantName = "Oxtail House"
+restaurantName = "One Oxtail"
+print(restaurantName)
+
+let city = "Tampa"
+print(city)
+
+var specialDish = "Oxtail Soup"
+var availableTables = 5
+var dishPrice = 15.99
+var isOpen = true
+
+print ("Today's special dish: \(specialDish) - $\(dishPrice)")
+
+
+var numberOfTables = 11
+let ownerName = "Doug Mahoney"
+
+print ("Oxtail House has \(numberOfTables) tables today, owned by \(ownerName)")
+
+
+let oxtailSoupPrice = 10.50
+let saladPrice = 7.50
+let subTotal = oxtailSoupPrice + saladPrice
+let tip = subTotal * 0.15
+let total = subTotal + tip
+
+print(total)
+
